63c037bd53601dfdc43c1fb1e4ff9e11c695d7c8
LARISSA CRISTINA GATTO GONCALVES <00001080852475sp@aluno.educacao.sp.gov.br>
2024-05-28 11:27:18 -0300

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+function somarNumeros(){
+   
+    parseInt( document.getElementById("num1").value)
+    var numero1 =  document.getElementById("num1").value;
+    var numero2 =  parseInt(document.getElementById("num2").value);
+    var resultado = numero1+numero2
+    alert (resultado)
+}
